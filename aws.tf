2adf8e7f60d9c05262b12ba6bd60d0ccc194fad6
Robbie T <github@rkst.it>
2019-10-03 15:03:58 -0700

    Updated assume_role

--- aws.tf
+++ aws.tf
@@ -2,7 +2,7 @@ provider "aws" {
   version = "~> 2.17"
   region = "us-east-1"
   assume_role {
-    role_arn     = "arn:aws:iam::985898799555:role/OrganizationAccountAccessRole"
+    role_arn     = "arn:aws:iam::709632896338:role/OrganizationAccountAccessRole"
     session_name = "TerraformDemo"
   }
 }
