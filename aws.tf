78aaee3493099bef8d4f416c0120a9890a34c383
Robbie T <github@rkst.it>
2019-10-03 15:06:38 -0700

    Removed role assume and remote block

--- aws.tf
+++ aws.tf
@@ -1,8 +1,4 @@
 provider "aws" {
   version = "~> 2.17"
   region = "us-east-1"
-  assume_role {
-    role_arn     = "arn:aws:iam::709632896338:role/OrganizationAccountAccessRole"
-    session_name = "TerraformDemo"
-  }
 }
--- terraform.tf
+++
@@ -1,9 +0,0 @@
-terraform {
-  backend "remote" {
-    hostname = "app.terraform.io"
-    organization = "Compu-Global-Hyper-Mega-Net"
-    workspaces {
-      name = "cost-estimation"
-    }
-  }
-}
